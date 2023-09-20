8d7ec8f249297025f3f5b57ee1a0c5a52eb1539d
Rafael Santos <rafaelsantos138@hotmail.com>
2023-09-20 15:35:19 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use("/api", router)
+
+const porta = process.env.PORT | 3000;
+
+server.listen(porta, () => {
+  console.log(`🚀 A API do NoteKepper está rodando na porta: ${porta}`)
+})
